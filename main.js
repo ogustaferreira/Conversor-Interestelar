6b36744efc64886d56e6178203a3e6d834be7b35
Gustavo Ferreira da Silva <gusta23clash@gmail.com>
2023-02-05 19:32:30 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var valorAnoLuz = prompt(
+    "Digite o valor em anos-luz para ser convertido em metros: "
+);
+
+var valorEmMetro = 9.461e15;
+
+var resultado = valorAnoLuz * valorEmMetro;
+
+alert(valorAnoLuz + " Anos-Luz equivale a " + resultado + " metros");
